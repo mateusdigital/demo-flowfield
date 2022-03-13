c06dcb1682bb53bc4c29751bcf31d2779c1655c3
stdmatt <stdmatt@pixelwizards.io>
2022-03-13 14:55:59 -0300

    [NEW] Basic flowfield ;))

--- source/main.js
+++ source/main.js
@@ -18,12 +18,12 @@
 
 //------------------------------------------------------------------------------
 __SOURCES = [
+    "/modules/demolib/modules/external/perlin.js",
     "/modules/demolib/source/demolib.js",
 ]
 
 const C = {}; // Constants
 const G = {}; // Globals
-
 
 
 //----------------------------------------------------------------------------//
@@ -32,7 +32,7 @@ const G = {}; // Globals
 //------------------------------------------------------------------------------
 function setup_demo_mode()
 {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject)=> {
         demolib_load_all_scripts(__SOURCES).then(()=> {
             canvas = document.createElement("canvas");
 
@@ -50,21 +50,6 @@ function setup_demo_mode()
 }
 
 //------------------------------------------------------------------------------
-function setup_common(canvas)
-{
-    set_main_canvas(canvas);
-    set_canvas_fill("white");
-
-    set_random_seed       (null);
-    install_input_handlers(canvas);
-
-    // :)
-
-    translate_canvas_to_center();
-    start_draw_loop(update_demo);
-}
-
-//------------------------------------------------------------------------------
 function demo_start(user_canvas)
 {
     if(!user_canvas) {
@@ -76,11 +61,74 @@ function demo_start(user_canvas)
     }
 }
 
+
+
+//------------------------------------------------------------------------------
+function setup_common(canvas)
+{
+    set_random_seed(null);
+    set_noise_seed (null);
+
+    set_main_canvas       (canvas);
+    install_input_handlers(canvas);
+   
+    set_canvas_fill  ("white");
+    set_canvas_stroke("white");
+
+
+    //
+    // Init :)
+    //
+
+    C.MAX_PARTICLES = 100;
+    G.particles     = [];
+
+    const canvas_w = get_canvas_width ();
+    const canvas_h = get_canvas_height();
+    for(let i = 0; i < C.MAX_PARTICLES; ++i) {
+        const x = random_int(canvas_w);
+        const y = random_int(canvas_h);
+        const p = make_vec2(x, y);
+        G.particles.push(p);
+    }
+
+
+    start_draw_loop(update_demo);
+}
+
 //------------------------------------------------------------------------------
 function update_demo(dt)
 {
     clear_canvas();
     begin_draw();
-        // :)
-    end_draw();
+
+        const canvas_w    = get_canvas_width ();
+        const canvas_h    = get_canvas_height();
+        const noise_scale = 0.0001;
+
+        set_canvas_stroke("red");
+        for(let i = 0; i < G.particles.length; ++i) {
+            const p = G.particles[i];
+            // const x = r * Math.cos((i / G.particles.length) * MATH_2PI); // p.x;
+            // const y = r * Math.sin((i / G.particles.length) * MATH_2PI); // p.x;
+            const n = perlin_noise(p.x * noise_scale, p.y * noise_scale);
+            const a = (n * MATH_2PI);
+            
+            const xx = Math.cos(a) * 50;
+            const yy = Math.sin(a) * 50;
+
+            draw_point(p.x, p.y, 2);
+            draw_line(p.x, p.y, p.x + xx, p.y + yy);
+
+            p.x += (xx * dt);
+            p.y += (yy * dt);
+            
+            if(p.x < 0 || p.x >= canvas_w || p.y < 0 || p.y > canvas_h) { 
+                p.x = random_int(canvas_w);
+                p.y = random_int(canvas_h);
+            }
+
+            G.particles[i] = p;
+        }
+    end_draw()
 }
